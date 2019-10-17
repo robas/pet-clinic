858152b7236533b744ff89a21ef85e637521e3e9
Robas <pax.profundis@gmail.com>
2019-10-17 19:23:32 -0300

    Adding POJOs. Closes #2.

---
+++ src/main/java/br/com/robas/petclinic/model/Person.java
@@ -0,0 +1,24 @@
+package br.com.robas.petclinic.model;
+
+public class Person {
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String lastName;
+
+}
---
+++ src/main/java/br/com/robas/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package br.com.robas.petclinic.model;
+
+public class Vet extends Person {
+}
