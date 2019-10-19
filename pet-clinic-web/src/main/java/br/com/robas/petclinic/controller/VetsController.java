c1385fb38aee690e988db729b2cd70b3a824e40e
Robas <pax.profundis@gmail.com>
2019-10-19 20:43:42 -0300

    Added Vets list and updated Vets controller. Closes #16

--- pet-clinic-web/src/main/java/br/com/robas/petclinic/controller/VetsController.java
+++ pet-clinic-web/src/main/java/br/com/robas/petclinic/controller/VetsController.java
@@ -1,14 +1,22 @@
 package br.com.robas.petclinic.controller;
 
+import br.com.robas.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
 
-    @RequestMapping({"/vets", "/vets/index.html", "/vets/index"})
-    public String listVets() {
+    private final VetService vetService;
 
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/index.html", "/vets/index"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
