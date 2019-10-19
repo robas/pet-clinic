6ea69f3bcec9db9dc0b565930214354d4758a168
Robas <pax.profundis@gmail.com>
2019-10-19 20:35:00 -0300

    Added Owners list and updated Owners controller. Closes #17

--- pet-clinic-web/src/main/java/br/com/robas/petclinic/controller/OwnersController.java
+++ pet-clinic-web/src/main/java/br/com/robas/petclinic/controller/OwnersController.java
@@ -1,14 +1,22 @@
 package br.com.robas.petclinic.controller;
 
+import br.com.robas.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/owners"})
 @Controller
 public class OwnersController {
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"", "/", "/index.html", "/index"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
